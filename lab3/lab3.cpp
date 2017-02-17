8e9e76fef7e6d8ce222171e7477a8273b10d8a0c
Matthew Wong <matthew.wong@outlook.com>
2017-02-17 00:57:47 +0000

    Save code after hardcode inserting

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -10,99 +10,118 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 #include <climits>
 using namespace std;
 
 int main(){
+
 /*
 Complete formula
 p(n+1) = (1+g-h)p(n) - gp(n)^2/M.
 
 */
-int initPop,years,maxPop,finalPop,initf;
-//p = initial population --------- p(n) is population for year n
-//n = year
-//m = maximum population
 
-double growthRate,lossRate;
-//g = rate of growth
-//h = minimum loss rate --------- both are percentages/decimals
+int initPop,calcYears;
+//p (initPop) = initial population 
+//n (calcYears) = year 
+//m (maxPop) = maximum population 
+
+double growthRate,lossRate,maxPop,finalPop;
+//g (growthRate) = rate of growth 
+//h (lossRate) = minimum loss rate
 //finalPop = final population
 
 
 //population loses gp(n) every year
 
-while(true){
-    cout << "Enter the initial population: ";
-    cin >> initPop;
+/*
+Initial population must at least be greater than 1
+If not then loop
+*/
+
+do {
+	cout << "Enter the initial population: ";
+	cin >> initPop;
+
+	
+} while(initPop < 1);
+
+/*
+The number of years that are being calculated
+Must be greater than 0
+*/
+
+do {
+
+	cout << "Enter the number of years to calculate: ";
+	cin >> calcYears;
+	
+} while(calcYears < 0);
+
+/*
+The rate of growth and loss for the population
+User will input the rate in percentage form
+It will then be converted to decimal
+*/
+
+do {
+
+	cout << "Enter the rate of growth: (without %) ";
+	cin >> growthRate;
+	
+} while(growthRate <= 0);
+
+growthRate /= 100; //to convert into decimal format
+
+do {
+
+	cout << "Enter the rate of loss: (without %) ";
+	cin >> lossRate;
+	
+} while(lossRate <= 0);
+
+lossRate /= 100; //to convert into decimal format
+
+/*
+The maximum population
+*/
+
+do {
+
+	cout << "Enter the maximum population: ";
+	cin >> maxPop;
+
+	
+} while(maxPop < 1);
+
+
+// Formatting columns to look nice
+cout << setw(2) << left << "Year" << setw(25) << right << "Population" << endl;
+
+// No decimals
+cout << setprecision(0)<<fixed;
+
+//Hardcode for testing
+/*
+initPop = 10;
+calcYears = 1;
+maxPop = 100;
+
+growthRate = 2.5;
+lossRate = .2;
+*/
+for(int i=1;i <calcYears;i++){
+	if(i == 1)
+		finalPop = ((1 + growthRate - lossRate) * initPop * i) - (growthRate *((initPop * i) / maxPop));
+	else
+		finalPop = ((1 + growthRate - lossRate) * finalPop) - (growthRate * ((finalPop * finalPop) / maxPop));
+
+    //Keeps the value of the previous calculated population
+    cout << setw(2) << i << left  << setw(25)<< right << finalPop << endl;
     
-    if(initPop < 2){
-        cout << "Please enter a number that is at least greater than 2." << endl;
-        return 0;
-    }
-    
-    cout << "Enter the number of years to calculate: ";
-    cin >> years;
-    
-    if(initPop < 0){
-        cout << "Please enter a number that is at least greater than 0." << endl;
-        return 0;
-    }
-    
-    cout << "Enter the rate of growth: (Please enter in percentage format without the percent sign) ";
-    cin >> growthRate;
-    
-    if(growthRate < 0){
-        cout << "Please enter a number that is at least greater than 0." << endl;
-        return 0;
-    }
-    
-    cout << "Enter the rate of loss: (Please enter in percentage format without the percent sign) ";
-    cin >> lossRate;
-    
-    
-    if(lossRate < 0){
-        cout << "Please enter a number that is at least greater than 0." << endl;
-        return 0;    
-    }
-    
-    cout << "Enter the maximum population: ";
-    cin >> maxPop;
-    
-    if(maxPop <= 0){
-        cout << "Please enter a number that is at least greater than 0." << endl;
-        return 0; 
-    }
-    
-    //To covert user input to decimal format
-    growthRate /= 100;
-    lossRate /= 100;
-    
-    
-    /*
-    p = 10;
-    n = 1;
-    m = 100;
-    
-    g = 2.5;
-    h = 0.2;
-    */
-    
-    cout << setw(2) << left << "Year" << setw(25) << right << "Population" << endl;
-    
-    for(int i=1;i <years;i++){
-    	if(i==1)
-    		finalPop = ((1 + growthRate - lossRate) * initPop * i) - (growthRate *((initPop * i) / maxPop));
-    	else
-    		finalPop = ((1+growthRate - lossRate) * finalPop) - (growthRate * ((finalPop * finalPop) / maxPop));
-    
-        initf = finalPop; //Keeps the value of the previous calculated population
-        cout << setw(2) << i << left  << setw(25)<< right << finalPop << endl;
-        
-    }
-    return 0;
 }
+return 0;
+
 
 
 }
