4a6f0b42d71c239561657cc84c4465366cdd40a9
Matthew  Wong <Matthew.Wong27@cslab22.cs.hunter.cuny.edu>
2017-03-09 21:55:02 -0500

    Modified lab 5 now comment

--- lab5/lab5b.cpp
+++ lab5/lab5b.cpp
@@ -12,30 +12,37 @@
 #include <cmath>
 using namespace std;
 
-int digitAtIndex(int, int);
-int numDigits(int);
-int divisibleByNine(int);
+// Precondition: num > 0 and (0 < index < number of Digits - 1)
+// Postcondition: the return value is the iterated sum of digits of num
+int getDigit(int num, int index);
 
-int digitAtIndex(int num, int index)  // return the index'th digit of num   97534 (4 indicies, to get index 2, )
+// Precondition: num > 0
+// Postcondition: the return value is the number of digits of the input number
+int numDigits(int num);
+
+// Precondition: num > 0
+// Postcondition: the return value will make the input divisble by 9 not if not divisible already
+int transformNum(int num);
+
+// Precondition: num > 0
+// Postcondition: the return value is the iterated sum of digits of num
+int sumDigits(int num);
+
+int getDigit(int num, int index)  // return the index'th digit of num   97534 (4 indicies, to get index 2, )
 {
-  // cout << "digitAtIndex function" << endl;
   int padding = 10;
   int nextpad, leftover;
   int indexStop = numDigits(num) - index;
-  // cout << "indexStop: " << indexStop << endl;
   int numberOfDigits = numDigits(num);
 
   for (int i = numberOfDigits; i > 0; i--) {
     nextpad = pow(padding, i - 1);
-    // cout << "nextpad value: " << nextpad << endl;
     leftover = num / nextpad;
-    // cout << "Leftover: " << leftover << endl;
 
     if (i == indexStop) {
       return leftover % 10;
     }
   }
-  // cout << "end digitAtIndex function" << endl;
 }
 
 int numDigits(int num)  // return the number of digits in num
@@ -52,65 +59,56 @@ int numDigits(int num)  // return the number of digits in num
 }
 
 int transformNum(int num) {
+
   int leftover = num % 9;
-  // cout << "leftover: " << leftover << endl;
-  int lastDigit = digitAtIndex(num, numDigits(num) - 1);
-  // cout << "last digit: " << lastDigit << endl;
-  int leftPlusLastDigit = lastDigit + leftover;
-  // cout << "last addition digit: " << leftPlusLastDigit << endl;
-  if (leftover == 0) return num;
-
-  int getNine = (9 - lastDigit) + num;
-
-  for (int i = 0; i <= 9; i++) {
-    if (getNine % 9 == 0) return getNine;
-
-    cout << "------------------" << endl;
-    cout << "i " << i << endl;
-    cout << "modulo " << ((getNine - i) % 9) << endl;
-    cout << "------------------" << endl;
-    if ((getNine - i) % 9 == 0) {
-      getNine -= i;
-      cout << "Second if " << ((getNine - i) % 9) << endl;
-      return getNine;
-    }
+  int lastDigit = getDigit(num, numDigits(num));
+  int result;
+  int difference = (leftover - lastDigit);
+  
+  if(leftover == 0)
+    return num;
+  if(num < 9)
+    return 9;
+  if(difference < 5)
+  {
+    if(lastDigit < 4)
+      result = (num - leftover);
+    if(lastDigit >= 5)
+      result = (num - (9 - leftover));
   }
+  else
+  {
+    if(lastDigit < 4)
+      result = (num + (9 - leftover));
+    if(lastDigit >= 5)
+      result = (num - (9 - leftover));
+  }
+    
+  return result;
+ 
 }
-
-  /*
-  else if(leftPlusLastDigit < 5)
-  {
-    num -= leftover;
-
-    cout << "First else if" << endl;
-    return num;
-  }
-  else if(leftPlusLastDigit > 5)
-  {
-    num += (leftPlusLastDigit - leftover);
-
-    cout << "Second else if" << endl;
-    return num;
-  }
-  else if()
-  */
 
 int sumDigits(int num) {
   int loops = numDigits(num);
   int result = 0;
   if (num < 10) return num;
   for (int i = 0; i < loops; i++) {
-    result += digitAtIndex(num, i);
+    result += getDigit(num, i);
   }
-  return result;
+  if(result > 10)
+    sumDigits(result);
+  else
+    return result;
 }
+
 int main() {
   int number;
 
   cout << "Enter number: ";
   cin >> number;
-  cout << sumDigits(number);
-  cout << transformNum(number) << endl;
+  cout << "The sum of the digits is: " << sumDigits(number) << endl;
   
+  cout << transformNum(number) << " will now by divisble by 9." << endl;
+  //cout << "Last digit is: " << getDigit(number, numDigits(number) - 1) << endl;
   return 0;
 }
