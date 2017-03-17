83ff23cdf4c789ffa1aa74d95c8402c5c1a40fd9
Matt <matthew.wong@outlook.com>
2017-03-17 21:13:28 +0000

    benchmark time

--- CS135_HW/sentences.cpp
+++ CS135_HW/sentences.cpp
@@ -17,18 +17,79 @@ Creating and checking sentences
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
+
+// int nvn(string, ifstream, ifstream);
 /*
-int nvn(string, ifstream, ifstream, ifstream)
 int nvan(string, ifstream, ifstream, ifstream, ifstream)
 int pvn(string, ifstream, ifstream, ifstream, ifstream)
 int pvan(string, ifstream, ifstream, ifstream, ifstream)
 int pvp(string, ifstream, ifstream)
-int nvn(sentence, nouns, verbs) {
+*/
+
+
+int nvn(string sentence) {
+  // <sentence> ::= <noun><verb><noun>
+  string compare, noun1, verb, noun2;
+  bool done = false;
+  ifstream nouns2, nouns, verbs;
+  nouns.open("nouns");
+  
+
+  clock_t begin = clock();
+  while( nouns >> noun1 ){
+    
+    verbs.open("verbs");
+    while( verbs >> verb ){
+      nouns2.open("nouns");
+      while( nouns2 >> noun2 ){
+
+        compare = noun1 + " " + verb + " " + noun2;
+
+        if(compare == sentence)
+        {
+          cout << "Compare: " << compare << endl;
+          cout << "Sentence: " << sentence << endl;
+            clock_t end = clock();
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "time taken for function() " << elapsed_secs << " seconds.\n";
+          return 1;
+        }
+        
+
+
+      }
+      nouns2.close();
+    }
+    verbs.close();
+  }
+  
+  /*
+  while(getline(nouns, noun1)) {
+
+    while(getline(verbs, verb)) {
+
+      while(getline(nouns2, noun2)) {
+        
+      compare = noun1 + " " + verb + " " + noun2;
+      if(compare == sentence)
+        return 1;
+        
+      }
+
+    }
+    
+  }
+  */
+
+  cout << compare + "\n";
+  nouns.close();
   return 0;
 }
 
+/*
 int nvan(sentence, nouns, verbs, adjectives) {
   return 0;
 }
@@ -44,39 +105,43 @@ int pvan(sentence, pronouns, verbs, adjectives, nouns) {
 int pvp(sentence, pronouns, verbs) {
   return 0;
 }
-
-int checkSentence(sentence, verbs, nouns, adjectives, pronouns) {
-  int nvn, nvan, pvn, pvan, pvp;
-  nvn = nvn(sentence, nouns, verbs);
-  nvan = nvan(sentence, nouns, verbs, adjectives);
-  pvn = pvn(sentence, pronouns, verbs, nouns);
-  pvan = pvan(sentence, pronouns, verbs, adjectives, nouns);
-  pvp = pvp(sentence, pronouns, verbs);
-
-  if(nvn || pvn || pvan || pvp)
-    return 1;
-  else
-    return 0;
-}
 */
 
+void checkSentence(string sentence) {
+  int nvn_check, nvan_check, pvn_check, pvan_check, pvp_check;
+  nvn_check = nvn(sentence);
+  //nvan = nvan(sentence, nouns, verbs, adjectives);
+  //pvn = pvn(sentence, pronouns, verbs, nouns);
+  //pvan = pvan(sentence, pronouns, verbs, adjectives, nouns);
+  //pvp = pvp(sentence, pronouns, verbs);
+
+  if(nvn_check /*|| pvn || pvan || pvp*/)
+    cout << "Valid sentence\n";
+  else
+    cout << "Invalid sentence\n";
+  /*
+  verbs.close();
+  nouns.close();
+  adjectives.close();
+  pronouns.close();
+  */
+}
+
+
 int main() {
-	string sentence, line;
-  int x, sum = 0, n = 0;
-  ofstream verbs, nouns, adjectives, pronouns;
-  verbs.open("verbs");
-  nouns.open("nouns");
-  adjectives.open("adjectives");
-  pronouns.open("pronouns");
+	string sentence;
+  //ifstream verbs, nouns, adjectives, pronouns;
+  //verbs.open("verbs");
+  //nouns.open("nouns");
+  //adjectives.open("adjectives");
+  //pronouns.open("pronouns");
 	cout << "Insert your sentence \n";
 	getline(cin,sentence);
-  //cout << sentence;
+  //cout << sentence << " Main \n";
 
-  cout << verbs;
+  checkSentence(sentence);
 
-  while ( getline (verbs, line) )
-    {
-      cout << line << '\n';
-    }
+  return 0;
+  
   //checkSentence(sentence, verbs, nouns, adjectives, pronouns);
 }
