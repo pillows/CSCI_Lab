0471d2f172954b9729aa485927d2b8439337ff97
Matt <matthew.wong@outlook.com>
2017-04-12 18:52:16 +0000

    Lab 8 + Bot

--- CS135_HW2/bot.cpp
+++ CS135_HW2/bot.cpp
@@ -37,13 +37,14 @@ void surroundingShips(vector<vector<int> > grid){
 
 }
 
-void impossibleSpaces(vector<vector<int> > grid, int cols, int r, int c, Screen &screen){
+/*
+void impossibleSpaces(vector<vector<int> > grid, int cols, int rows, int r, int c, Screen &screen){
   //int start = 1;
   //bool single;
   if(grid[r][c] == 2){
 
     // If the row will go out of bounds then make sure it's not
-    if( !((r+1) >= (int)grid[r].size()) ){
+    if( !((r+1) >= rows) ){
       if(grid[r+1][c] == 0 || grid[r+1][c] == 5){
         grid[r+1][c] = 0;
         screen.mark(r+1, c, 'X', GREEN);
@@ -76,6 +77,46 @@ void impossibleSpaces(vector<vector<int> > grid, int cols, int r, int c, Screen 
       return;
 
   }
+}
+*/
+
+void impossibleSpaces(vector<vector<int> > grid, int cols, int rows, int r, int c, Screen &screen, ostream &log){
+  log << "impossibleSpaces" << endl;
+  int left, right, top, bottom;
+  left = r - 1;
+  right = r + 1;
+  top = c + 1;
+  bottom = c - 1;
+
+  if(left < 0)
+    left++;
+  if(right > COLS)
+    right--;
+  if(top < 0)
+    top++;
+  if(bottom > ROWS)
+    bottom--;
+
+  log << "left: ("<< left << "," << c << ")\n";
+  log << "right: ("<< right << "," << c << ")\n";
+  log << "top: ("<< r << "," << top << ")\n";
+  log << "bottom: ("<< r << "," << bottom << ")\n";
+
+  
+  if( (grid[left][c] == 5 || grid[left][c]== 0 ) && (grid[right][c] == 0 || grid[right][c] == 5) && (grid[r][top] == 0 || grid[r][top] == 5) && (grid[r][bottom] == 0 || grid[r][bottom] == 5) ){
+    grid[left][c] = 0;
+    screen.mark(left, c, 'X', GREEN);
+
+    grid[right][c] = 0;
+    screen.mark(right, c, 'X', GREEN);
+
+    grid[r][top] = 0;
+    screen.mark(r, top, 'X', GREEN);
+
+    grid[r][bottom] = 0;
+    screen.mark(r, bottom, 'X', GREEN);
+  }
+  
 }
 
 
@@ -155,7 +196,79 @@ void impossibleSpaces(vector<vector<int> > grid, int cols, int r, int c, Screen 
     log      = a cout-like output stream
 */
 
-void continousShot(vector<vector<int> > grid, int r, int c, Gun &gun, Screen &screen, ostream &log);
+bool checkBadCoordinate(vector<vector<int> > grid, int r, int c){
+  if(grid[r][c] == 5 || grid[r][c] == 0)
+    return true;
+  else
+    return false;
+}
+
+void continousShot(vector<vector<int> > grid, int ROWS, int COLS, int r, int c, Gun &gun, Screen &screen, ostream &log){
+  log << "cont shoot" << endl;
+  int left, right, top, bottom;
+  int contshot = 1;
+  bool stopshot = true;
+  left = r - 1;
+  right = r + 1;
+  top = c + 1;
+  bottom = c - 1;
+
+
+
+  if(left < 0)
+    left++;
+  if(right > COLS)
+    right--;
+  if(top < 0)
+    top++;
+  if(bottom > ROWS)
+    bottom--;
+
+  log << "left: ("<< left << "," << c << ")\n";
+  log << "right: ("<< right << "," << c << ")\n";
+  log << "top: ("<< r << "," << top << ")\n";
+  log << "right: ("<< r << "," << bottom << ")\n";
+
+  result res = gun.shoot(left,c);
+  if(res == HIT || res == HIT_N_SUNK){
+    if(res == HIT)
+      screen.mark(left, c, '@', GREEN);
+    if(res == HIT_N_SUNK)
+      screen.mark(left, c, 'S', RED); 
+
+    grid[left][c] = res;
+    
+    while(stopshot){
+      result res2 = gun.shoot(left-contshot,c);
+      if(res2 == HIT){
+        grid[left-contshot][c] = 1;
+        grid[left-contshot][c+1] = 0;
+        screen.mark(left-contshot, c+1, 'X', GREEN);
+        grid[left-contshot][c-1] = 0;
+        screen.mark(left-contshot, c-1, 'X', GREEN);
+      }
+
+      else if(res2 == HIT_N_SUNK){
+        grid[left-contshot][c] = 2;
+        screen.mark(left-contshot, c+1, 'S', RED);
+        stopshot = true;
+      } 
+      else if(res2 == MISS){
+        grid[left-contshot][c] = 0;
+        screen.mark(left-contshot, c, 'X', GREEN);
+        stopshot = true;
+      }
+    }
+    
+  }
+  else if(res == MISS){
+    grid[left][c] = 0;
+  }
+
+
+
+}
+
 void next_turn(int sml, int lrg, int num, Gun &gun, Screen &screen, ostream &log)
 {
   bool unique = true;
@@ -182,11 +295,12 @@ void next_turn(int sml, int lrg, int num, Gun &gun, Screen &screen, ostream &log
   else if (res == HIT) {
       screen.mark(r, c, '@', GREEN); 
       grid[r][c] = 1;
+      continousShot(grid,ROWS,COLS,r,c,gun,screen,log);
   }
 
   else if (res == HIT_N_SUNK){
       screen.mark(r, c, 'S', RED); 
       grid[r][c] = 2;
-      impossibleSpaces(grid,COLS,r,c,screen);
+      impossibleSpaces(grid,COLS,ROWS,r,c,screen,log);
   }
 }
